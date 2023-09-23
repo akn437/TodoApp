13b878602feefe8812dcc3aefde8d6da0117b423
Antony Kevin <Antony.Kevin@soti.net>
2023-09-24 01:04:29 +0530

    Example Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TodoApp';
+  readonly APIUrl="http://localhost:5275/api/todoapp/"; 
+  constructor(private http:HttpClient)
+  {
+
+  }
+  notes:any=[];
+  refreshNotes()
+  {
+    this.http.get(this.APIUrl+'GetNotes').subscribe(data=>{
+      this.notes=data;
+    })
+  }
+
+
+  ngOnInit()
+  {
+     this.refreshNotes();
+  }
+  
+  addNotes()
+  {
+    var newNotes=(<HTMLInputElement>document.getElementById("newNotes")).value;
+    var formData=new FormData();
+    formData.append("newNotes",newNotes);
+    this.http.post(this.APIUrl+'AddNotes',formData).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+  })
+  }
+
+  deleteNotes(id:any)
+  {
+    this.http.delete(this.APIUrl+'DeleteNotes?id='+id).subscribe(data=>{
+      alert(data);
+      this.refreshNotes();
+  })
+  }
+ 
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
